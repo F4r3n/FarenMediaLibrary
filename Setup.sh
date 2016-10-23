04e49d3b36226af8221054a738f970bb4ccd25cc
F4r3n <guillaume_k@hotmail.fr>
2016-10-23 20:52:13 +0200

    build

---
+++ Setup.sh
@@ -0,0 +1,40 @@
+sudo rm -rf temp_build
+mkdir temp_build
+cd temp_build
+sudo apt-get install -qq libglew-dev libopenal-dev
+
+if [ -a "/usr/local/lib/libsndfile.so" ]
+then
+    echo "Lib sndfile found"
+else
+    cd ../
+    wget http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.27.tar.gz
+    tar zxf libsndfile-1.0.27.tar.gz
+    cd libsndfile-1.0.27
+    ./configure
+    sudo make install
+fi
+if [ -a "/usr/local/lib/liblua.a" ]
+then
+    echo "Lib lua found"
+else
+    curl -R -O http://www.lua.org/ftp/lua-5.3.3.tar.gz
+    tar zxf lua-5.3.3.tar.gz
+    cd lua-5.3.3
+    sudo make linux install MYCFLAGS='-fPIC'
+fi
+cd ../
+echo $PWD
+git submodule init && git submodule update
+cd extlib/glfw
+cmake .
+sudo make install
+cd ../freetype2
+mkdir build
+cd build
+cmake -D BUILD_SHARED_LIBS:BOOL=true .. && make
+cd extlib/Box2D
+cmake .. -DCMAKE_CXX_FLAGS="-std=c++11" -DBOX2D_BUILD_SHARED=ON -DBOX2D_BUILD_EXAMPLES=OFF
+sudo make install
+cd ../../../ 
+
